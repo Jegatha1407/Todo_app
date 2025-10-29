8975e4e4123ded079c34967898168babdb4e27ca
Jegatha1407 <169113140+Jegatha1407@users.noreply.github.com>
2025-10-29 08:09:11 -0700

    Delete todo-app directory

--- todo-app/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_unset();  // remove all session variables
-session_destroy(); // destroy the session completely
-
-// Redirect to login page
-header("Location: login.php");
-exit();
-?>
